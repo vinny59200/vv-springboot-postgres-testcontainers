1a467ac5ec2ceeb0e2637a1ac4f45c6f31ef3be4
vvauban <vvauban@gmail.com>
2023-07-14 19:40:01 +0200

    Add constructors in Customer Entity

--- src/main/java/com/vv/testcontainerpostgres/customer/model/Customer.java
+++ src/main/java/com/vv/testcontainerpostgres/customer/model/Customer.java
@@ -1,10 +1,14 @@
 package com.vv.testcontainerpostgres.customer.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
     private Integer id;
